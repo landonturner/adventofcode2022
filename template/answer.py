67520227453eff82195e9e68404a4c43858d8a18
Landon Turner <landon@lturner.net>
2022-12-05 10:18:04 -0600

    feat: update template to allow libs

--- template/answer.py
+++ template/answer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-with open('input.txt', 'r') as f:
-    lines = [l.strip() for l in f.readlines()]
+def run(lines):
+    print(lines)
 
-print(lines)
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        lines = [l.strip() for l in f.readlines()]
+    run(lines)
